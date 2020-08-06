132c63f5f87529dd870c3a7693b93df2120f8d90
MoriyaShiine <moriyashiine@gmail.com>
2020-08-06 15:10:29 -0500

    basic vampire stuff

--- src/main/java/moriyashiine/bewitchment/common/registry/BWTreeDecoratorTypes.java
+++ src/main/java/moriyashiine/bewitchment/common/registry/BWTreeDecoratorTypes.java
@@ -2,7 +2,6 @@ package moriyashiine.bewitchment.common.registry;
 
 import moriyashiine.bewitchment.common.Bewitchment;
 import moriyashiine.bewitchment.common.world.generator.decorator.LeaveSpanishMossTreeDecorator;
-import moriyashiine.bewitchment.mixin.TreeDecoratorTypeAccessor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.tree.TreeDecorator;
@@ -14,7 +13,7 @@ import java.util.Map;
 public class BWTreeDecoratorTypes {
 	private static final Map<TreeDecoratorType<TreeDecorator>, Identifier> TREE_DECORATOR_TYPES = new LinkedHashMap<>();
 	
-	public static final TreeDecoratorType<TreeDecorator> LEAVE_SPANISH_MOSS = create("leave_spanish_moss", TreeDecoratorTypeAccessor.createTreeDecoratorType(LeaveSpanishMossTreeDecorator.CODEC));
+	public static final TreeDecoratorType<TreeDecorator> LEAVE_SPANISH_MOSS = create("leave_spanish_moss", new TreeDecoratorType<>(LeaveSpanishMossTreeDecorator.CODEC));
 	
 	private static TreeDecoratorType<TreeDecorator> create(String name, TreeDecoratorType<TreeDecorator> decorator) {
 		TREE_DECORATOR_TYPES.put(decorator, new Identifier(Bewitchment.MODID, name));
---
+++ src/main/java/moriyashiine/bewitchment/mixin/VampireHandler.java
@@ -0,0 +1,45 @@
+package moriyashiine.bewitchment.mixin;
+
+import moriyashiine.bewitchment.api.BewitchmentAPI;
+import moriyashiine.bewitchment.common.registry.BWDamageSources;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class VampireHandler extends Entity {
+	@Shadow public abstract boolean damage(DamageSource source, float amount);
+	
+	public VampireHandler(EntityType<?> type, World world) {
+		super(type, world);
+	}
+	
+	@Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+	private void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> callbackInfo) {
+		if (BewitchmentAPI.isVampire(this) && source.isFire()) {
+			callbackInfo.setReturnValue(damage(BWDamageSources.SUN, amount * 2));
+		}
+	}
+	
+	@Mixin(Entity.class)
+	private static class CancelFireImmunity
+	{
+		@Inject(method = "isFireImmune", at = @At("RETURN"), cancellable = true)
+		private void isFireImmune(CallbackInfoReturnable<Boolean> callbackInfo)
+		{
+			Object obj = this;
+			//noinspection ConstantConditions
+			if (BewitchmentAPI.isVampire((Entity) obj))
+			{
+				callbackInfo.setReturnValue(false);
+			}
+		}
+	}
+}
