e9ec4520eadb3d23856677617d36f330e475d79f
MoriyaShiine <moriyashiine@gmail.com>
2020-12-24 22:12:24 -0600

    entities avoid cauldrons

---
+++ src/main/java/moriyashiine/bewitchment/mixin/LandPathNodeMakerMixin.java
@@ -0,0 +1,20 @@
+package moriyashiine.bewitchment.mixin;
+
+import moriyashiine.bewitchment.common.block.WitchCauldronBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
+import net.minecraft.entity.ai.pathing.PathNodeMaker;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LandPathNodeMaker.class)
+public abstract class LandPathNodeMakerMixin extends PathNodeMaker {
+	@Inject(method = "method_27138", at = @At("HEAD"), cancellable = true)
+	private static void method_27138(BlockState blockState, CallbackInfoReturnable<Boolean> callbackInfo) {
+		if (blockState.getBlock() instanceof WitchCauldronBlock) {
+			callbackInfo.setReturnValue(true);
+		}
+	}
+}
