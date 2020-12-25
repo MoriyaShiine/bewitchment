b757109fb37c7cfebcee0e7cfb7cccb03cb28f33
MoriyaShiine <moriyashiine@gmail.com>
2020-12-25 10:55:27 -0600

    make crafted oils clearer

--- src/main/java/moriyashiine/bewitchment/common/block/entity/WitchCauldronBlockEntity.java
+++ src/main/java/moriyashiine/bewitchment/common/block/entity/WitchCauldronBlockEntity.java
@@ -22,6 +22,7 @@ import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -154,6 +155,9 @@ public class WitchCauldronBlockEntity extends BlockEntity implements BlockEntity
 					if (fluidHeight > 0) {
 						if (mode == Mode.TELEPORTATION) {
 							world.addParticle(new DustParticleEffect(((color >> 16) & 0xff) / 255f, ((color >> 8) & 0xff) / 255f, (color & 0xff) / 255f, 1), pos.getX() + 0.5 + MathHelper.nextDouble(world.random, -width, width), pos.getY() + fluidHeight, pos.getZ() + 0.5 + MathHelper.nextDouble(world.random, -width, width), 0, 0, 0);
+						}
+						if (mode == Mode.OIL_CRAFTING && color != 0xff3fff) {
+							world.addParticle(ParticleTypes.ENCHANTED_HIT, pos.getX() + 0.5 + MathHelper.nextDouble(world.random, -width, width), pos.getY() + fluidHeight, pos.getZ() + 0.5 + MathHelper.nextDouble(world.random, -width, width), 0, 0, 0);
 						}
 						world.addParticle((ParticleEffect) BWParticleTypes.CAULDRON_BUBBLE, pos.getX() + 0.5 + MathHelper.nextDouble(world.random, -width, width), pos.getY() + fluidHeight, pos.getZ() + 0.5 + MathHelper.nextDouble(world.random, -width, width), ((color >> 16) & 0xff) / 255f, ((color >> 8) & 0xff) / 255f, (color & 0xff) / 255f);
 					}
