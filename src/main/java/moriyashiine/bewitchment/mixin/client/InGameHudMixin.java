a13ad3fafb5c36007b1e7efe4b611f827d3664df
MoriyaShiine <moriyashiine@gmail.com>
2020-12-26 19:13:20 -0600

    fix rendering

--- src/main/java/moriyashiine/bewitchment/mixin/client/InGameHudMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/client/InGameHudMixin.java
@@ -41,7 +41,8 @@ public abstract class InGameHudMixin extends DrawableHelper {
 				if (magicAccessor.getMagicTimer() > 0) {
 					client.getTextureManager().bindTexture(BEWITCHMENT_GUI_ICONS_TEXTURE);
 					RenderSystem.color4f(1, 1, 1, magicAccessor.getMagicTimer() / 10f);
-					drawTexture(matrices, 13, (scaledHeight - 74) / 2, 25, 0, 7, (int) (74 - (magicAccessor.getMagic() * 74f / MagicAccessor.MAX_MAGIC)));
+					drawTexture(matrices, 13, (scaledHeight - 74) / 2, 25, 0, 7, 74);
+					drawTexture(matrices, 13, (scaledHeight - 74) / 2, 32, 0, 7, (int) (74 - (magicAccessor.getMagic() * 74f / MagicAccessor.MAX_MAGIC)));
 					drawTexture(matrices, 4, (scaledHeight - 102) / 2, 0, 0, 25, 102);
 					client.getTextureManager().bindTexture(GUI_ICONS_TEXTURE);
 				}
