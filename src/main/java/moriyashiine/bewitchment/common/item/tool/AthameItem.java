aeef85f0b811d99d6258011a4771c9c0e82acade
MoriyaShiine <moriyashiine@gmail.com>
2020-12-27 12:30:40 -0600

    athame dispenser behavior

--- src/main/java/moriyashiine/bewitchment/common/item/tool/AthameItem.java
+++ src/main/java/moriyashiine/bewitchment/common/item/tool/AthameItem.java
@@ -1,9 +1,15 @@
 package moriyashiine.bewitchment.common.item.tool;
 
 import moriyashiine.bewitchment.api.registry.AthameStrippingRecipe;
+import moriyashiine.bewitchment.common.block.DragonsBloodLogBlock;
+import moriyashiine.bewitchment.common.registry.BWMaterials;
+import moriyashiine.bewitchment.common.registry.BWProperties;
 import moriyashiine.bewitchment.common.registry.BWRecipeTypes;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.PillarBlock;
+import net.minecraft.block.dispenser.DispenserBehavior;
+import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
@@ -11,13 +17,26 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class AthameItem extends SwordItem {
+	private static final DispenserBehavior DISPENSER_BEHAVIOR = new FallibleItemDispenserBehavior() {
+		@Override
+		protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+			World world = pointer.getWorld();
+			BlockPos pos = pointer.getBlockPos();
+			setSuccess(cutLog(world, pos.offset(pointer.getBlockState().get(Properties.FACING)), stack));
+			return stack;
+		}
+	};
+	
 	public AthameItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
 		super(toolMaterial, attackDamage, attackSpeed, settings);
+		DispenserBlock.registerBehavior(this, DISPENSER_BEHAVIOR);
 	}
 	
 	@Override
@@ -44,5 +63,19 @@ public class AthameItem extends SwordItem {
 			return ActionResult.success(world.isClient);
 		}
 		return super.useOnBlock(context);
+	}
+	
+	private static boolean cutLog(World world, BlockPos pos, ItemStack stack) {
+		BlockState state = world.getBlockState(pos);
+		if (state.getBlock() instanceof DragonsBloodLogBlock && state.get(BWProperties.NATURAL) && !state.get(BWProperties.CUT)) {
+			world.playSound(null, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1, 1);
+			world.setBlockState(pos, state.with(BWProperties.CUT, true));
+			stack.damage(1, world.random, null);
+			if (stack.getDamage() == BWMaterials.SILVER_TOOL.getDurability()) {
+				stack.decrement(1);
+			}
+			return true;
+		}
+		return false;
 	}
 }
