77e800f33ea2c2200c982727e10e4d97c319a4b1
MoriyaShiine <moriyashiine@gmail.com>
2020-12-30 15:49:50 -0600

    witchberry foods are always edible

--- src/main/java/moriyashiine/bewitchment/common/registry/BWObjects.java
+++ src/main/java/moriyashiine/bewitchment/common/registry/BWObjects.java
@@ -236,9 +236,9 @@ public class BWObjects {
 	public static final Item BOTTLE_OF_BLOOD = create("bottle_of_blood", new BottleOfBloodItem(gen().maxCount(1).recipeRemainder(Items.GLASS_BOTTLE)));
 	public static final Item GRILLED_GARLIC = create("grilled_garlic", new Item(gen().food(FoodComponents.BAKED_POTATO)));
 	public static final Item GARLIC_BREAD = create("garlic_bread", new Item(gen().food(FoodComponents.PUMPKIN_PIE)));
-	public static final Item WITCHBERRY = create("witchberry", new Item(gen().food(FoodComponents.CARROT)));
-	public static final Item WITCHBERRY_PIE = create("witchberry_pie", new Item(gen().food(FoodComponents.PUMPKIN_PIE)));
-	public static final Item WITCHBERRY_COOKIE = create("witchberry_cookie", new Item(gen().food(FoodComponents.COOKIE)));
+	public static final Item WITCHBERRY = create("witchberry", new Item(gen().food(BWFoodComponents.WITCHBERRY)));
+	public static final Item WITCHBERRY_PIE = create("witchberry_pie", new Item(gen().food(BWFoodComponents.WITCHBERRY_PIE)));
+	public static final Item WITCHBERRY_COOKIE = create("witchberry_cookie", new Item(gen().food(BWFoodComponents.WITCHBERRY_COOKIE)));
 	//spawn_egg
 	public static final Item OWL_SPAWN_EGG = create("owl_spawn_egg", new SpawnEggItem(BWEntityTypes.OWL, 0x7f3f00, 0xc0c0c0, gen()));
 	public static final Item RAVEN_SPAWN_EGG = create("raven_spawn_egg", new SpawnEggItem(BWEntityTypes.RAVEN, 0x3f3f3f, 0x000000, gen()));
