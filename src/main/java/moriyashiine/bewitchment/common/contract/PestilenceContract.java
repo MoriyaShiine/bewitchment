cbb9090b4a2aa0922de527856b4c0720ad393c80
MoriyaShiine <moriyashiine@gmail.com>
2021-01-07 08:38:42 -0600

    split contracts

---
+++ src/main/java/moriyashiine/bewitchment/common/contract/PestilenceContract.java
@@ -0,0 +1,38 @@
+package moriyashiine.bewitchment.common.contract;
+
+import moriyashiine.bewitchment.api.registry.Contract;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.mob.ZombieVillagerEntity;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.ServerWorldAccess;
+
+public class PestilenceContract extends Contract {
+	public PestilenceContract(boolean canBeGiven) {
+		super(canBeGiven);
+	}
+	
+	public void tick(LivingEntity target, boolean includeNegative) {
+		if (includeNegative && target.age % 100 == 0) {
+			target.world.getEntitiesByType(EntityType.VILLAGER, new Box(target.getBlockPos()).expand(8), villagerEntity -> true).forEach(villagerEntity -> {
+				if (target.getRandom().nextFloat() < 1 / 40f) {
+					ZombieVillagerEntity zombieVillager = villagerEntity.method_29243(EntityType.ZOMBIE_VILLAGER, false);
+					if (zombieVillager != null) {
+						zombieVillager.initialize((ServerWorldAccess) target.world, target.world.getLocalDifficulty(zombieVillager.getBlockPos()), SpawnReason.CONVERSION, new ZombieEntity.ZombieData(false, true), null);
+						zombieVillager.setVillagerData(villagerEntity.getVillagerData());
+						zombieVillager.setGossipData(villagerEntity.getGossip().serialize(NbtOps.INSTANCE).getValue());
+						zombieVillager.setOfferData(villagerEntity.getOffers().toTag());
+						zombieVillager.setXp(villagerEntity.getExperience());
+						if (!villagerEntity.isSilent()) {
+							target.world.syncWorldEvent(null, 1026, villagerEntity.getBlockPos(), 0);
+						}
+						villagerEntity.remove();
+					}
+				}
+			});
+		}
+	}
+}
--- src/main/java/moriyashiine/bewitchment/common/registry/BWContracts.java
+++ src/main/java/moriyashiine/bewitchment/common/registry/BWContracts.java
@@ -2,6 +2,9 @@ package moriyashiine.bewitchment.common.registry;
 
 import moriyashiine.bewitchment.api.registry.Contract;
 import moriyashiine.bewitchment.common.Bewitchment;
+import moriyashiine.bewitchment.common.contract.DeathContract;
+import moriyashiine.bewitchment.common.contract.PestilenceContract;
+import moriyashiine.bewitchment.common.contract.SlothContract;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -14,15 +17,15 @@ public class BWContracts {
 	public static final Contract LUST = create("lust", new Contract(true));
 	public static final Contract GLUTTONY = create("gluttony", new Contract(true));
 	public static final Contract GREED = create("greed", new Contract(true));
-	public static final Contract SLOTH = create("sloth", new Contract(true));
+	public static final Contract SLOTH = create("sloth", new SlothContract(true));
 	public static final Contract WRATH = create("wrath", new Contract(true));
 	public static final Contract ENVY = create("envy", new Contract(true));
 	public static final Contract PRIDE = create("pride", new Contract(true));
 	
 	public static final Contract FAMINE = create("famine", new Contract(true));
-	public static final Contract PESTILENCE = create("pestilence", new Contract(true));
+	public static final Contract PESTILENCE = create("pestilence", new PestilenceContract(true));
 	public static final Contract WAR = create("war", new Contract(true));
-	public static final Contract DEATH = create("death", new Contract(false));
+	public static final Contract DEATH = create("death", new DeathContract(false));
 	
 	//	public static final Contract HERESY = create("heresy", new Contract());
 	//	public static final Contract VIOLENCE = create("violence", new Contract());
