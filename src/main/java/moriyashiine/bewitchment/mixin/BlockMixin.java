1c4f0d5457fe00a55087e5e013b601b6bbd406c9
MoriyaShiine <moriyashiine@gmail.com>
2021-01-15 18:18:13 -0600

    fix treasure fortune

--- src/main/java/moriyashiine/bewitchment/mixin/BlockMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/BlockMixin.java
@@ -47,16 +47,16 @@ public abstract class BlockMixin {
 				if (fortuneAccessor.getFortune() != null) {
 					if (fortuneAccessor.getFortune().fortune == BWFortunes.TREASURE && world.random.nextFloat() < 1 / 25f) {
 						Set<ItemStack> treasure = new HashSet<>();
-						for (int i = 0; i < world.random.nextInt(3); i++) {
+						for (int i = 0; i < world.random.nextInt(1) + 1; i++) {
 							switch (world.random.nextInt(4)) {
 								case 0:
-									treasure.add(new ItemStack(Items.DIAMOND, MathHelper.nextInt(world.random, 0, 3)));
+									treasure.add(new ItemStack(Items.DIAMOND, MathHelper.nextInt(world.random, 1, 3)));
 								case 1:
-									treasure.add(new ItemStack(Items.GOLD_INGOT, MathHelper.nextInt(world.random, 0, 3)));
+									treasure.add(new ItemStack(Items.GOLD_INGOT, MathHelper.nextInt(world.random, 1, 5)));
 								case 2:
-									treasure.add(new ItemStack(Items.IRON_INGOT, MathHelper.nextInt(world.random, 0, 3)));
+									treasure.add(new ItemStack(Items.IRON_INGOT, MathHelper.nextInt(world.random, 2, 10)));
 								case 3:
-									treasure.add(new ItemStack(BWObjects.SILVER_INGOT, MathHelper.nextInt(world.random, 0, 3)));
+									treasure.add(new ItemStack(BWObjects.SILVER_INGOT, MathHelper.nextInt(world.random, 1, 5)));
 								default:
 									treasure.add(ItemStack.EMPTY);
 							}
