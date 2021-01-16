c8fb265128b4f32fd85729a1bbbe98d1e44b3922
MoriyaShiine <moriyashiine@gmail.com>
2021-01-16 10:58:26 -0600

    buff attack damage

--- src/main/java/moriyashiine/bewitchment/common/entity/living/BaphometEntity.java
+++ src/main/java/moriyashiine/bewitchment/common/entity/living/BaphometEntity.java
@@ -60,7 +60,7 @@ public class BaphometEntity extends BWHostileEntity implements Pledgeable {
 	}
 	
 	public static DefaultAttributeContainer.Builder createAttributes() {
-		return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 375).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 10).add(EntityAttributes.GENERIC_ARMOR, 6).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25).add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 0.75);
+		return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 375).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 12).add(EntityAttributes.GENERIC_ARMOR, 6).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25).add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 0.75);
 	}
 	
 	@Override
