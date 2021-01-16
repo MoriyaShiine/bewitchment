153666548c1c4ba45a9acf815690846c830eab19
MoriyaShiine <moriyashiine@gmail.com>
2021-01-16 13:47:35 -0600

    drench removes fire

--- src/main/java/moriyashiine/bewitchment/common/ritualfunction/MakeEntitiesWetRitualFunction.java
+++ src/main/java/moriyashiine/bewitchment/common/ritualfunction/MakeEntitiesWetRitualFunction.java
@@ -1,7 +1,10 @@
 package moriyashiine.bewitchment.common.ritualfunction;
 
+import moriyashiine.bewitchment.api.BewitchmentAPI;
 import moriyashiine.bewitchment.api.interfaces.WetAccessor;
 import moriyashiine.bewitchment.api.registry.RitualFunction;
+import net.minecraft.block.AbstractFireBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.Inventory;
@@ -19,8 +22,11 @@ public class MakeEntitiesWetRitualFunction extends RitualFunction {
 	
 	@Override
 	public void start(ServerWorld world, BlockPos pos, Inventory inventory) {
-		int radius = 2;
+		int radius = 3;
 		world.getEntitiesByClass(Entity.class, new Box(pos).expand(radius), Entity::isAlive).forEach(entity -> WetAccessor.of(entity).ifPresent(wetAccessor -> wetAccessor.setWetTimer(6000)));
+		BewitchmentAPI.getBlockPoses(pos, radius, foundPos -> world.getBlockState(foundPos).getBlock() instanceof AbstractFireBlock).forEach(foundPos -> {
+			world.setBlockState(foundPos, Blocks.AIR.getDefaultState());
+		});
 		super.start(world, pos, inventory);
 	}
 }
