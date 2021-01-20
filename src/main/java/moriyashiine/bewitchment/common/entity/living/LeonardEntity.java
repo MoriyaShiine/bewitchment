e72a23767b1a1daa81037525bbf1537ec49bcd83
Sunconure11 <eyespot7b@aol.com>
2021-01-20 02:23:29 -0500

    I've got to decrease my IQ.

--- src/main/java/moriyashiine/bewitchment/common/entity/living/LeonardEntity.java
+++ src/main/java/moriyashiine/bewitchment/common/entity/living/LeonardEntity.java
@@ -142,7 +142,6 @@ public class LeonardEntity extends BWHostileEntity implements Pledgeable {
 				}
 				if (timer % 600 == 0 && world.getEntitiesByType(EntityType.WITCH, new Box(getBlockPos()).expand(32), entity -> getUuid().equals(((MasterAccessor) entity).getMasterUUID())).size() < 3) {
 					summonMinions();
-					world.playSound(null, getBlockPos(), SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.HOSTILE, 1, 1);
 					swingHand(Hand.OFF_HAND);
 				}
 			}
