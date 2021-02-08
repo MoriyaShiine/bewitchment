3e871195d250c250b20707f6e91f54647fa71b3d
CammiePone <inazuma261@gmail.com>
2021-02-08 13:05:09 -0500

    made brooms not be stuck up your ass

--- src/main/java/moriyashiine/bewitchment/api/client/renderer/BroomEntityRenderer.java
+++ src/main/java/moriyashiine/bewitchment/api/client/renderer/BroomEntityRenderer.java
@@ -24,6 +24,7 @@ public abstract class BroomEntityRenderer<T extends BroomEntity> extends EntityR
 		matrices.push();
 		matrices.translate(0, -1, 0);
 		matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(90 - yaw + 90));
+		matrices.translate(0, 0, -0.35);
 		model.setAngles(entity, yaw, 0, entity.age + entity.getEntityId(), 0, 0);
 		model.render(matrices, vertexConsumers.getBuffer(model.getLayer(getTexture(entity))), light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
 		matrices.pop();
