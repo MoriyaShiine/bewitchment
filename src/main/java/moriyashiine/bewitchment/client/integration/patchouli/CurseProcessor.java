f1b58a2010d960320fa71e18eb92b7668ea07e32
Aelpecyem <aelpecyem@gmail.com>
2021-02-19 22:48:48 +0100

    minor curse entries

---
+++ src/main/java/moriyashiine/bewitchment/client/integration/patchouli/CurseProcessor.java
@@ -0,0 +1,38 @@
+package moriyashiine.bewitchment.client.integration.patchouli;
+
+import moriyashiine.bewitchment.common.recipe.CurseRecipe;
+import moriyashiine.bewitchment.common.registry.BWRecipeTypes;
+import moriyashiine.bewitchment.common.registry.BWRegistries;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import vazkii.patchouli.api.IComponentProcessor;
+import vazkii.patchouli.api.IVariable;
+import vazkii.patchouli.api.IVariableProvider;
+
+public class CurseProcessor implements IComponentProcessor {
+    protected CurseRecipe recipe;
+
+    @Override
+    public void setup(IVariableProvider variables) {
+        String recipeId = variables.get("recipe").asString();
+        RecipeManager manager = MinecraftClient.getInstance().world.getRecipeManager();
+        recipe = (CurseRecipe) manager.get(new Identifier(recipeId)).filter(recipe -> recipe.getType().equals(BWRecipeTypes.CURSE_RECIPE_TYPE)).orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public IVariable process(String key) {
+        if (key.equals("header")){
+            return IVariable.from(new TranslatableText("curse." + BWRegistries.CURSES.getId(recipe.curse).toString().replace(":", ".")));
+        }
+        for (int i = 0; i < recipe.input.size(); i++) {
+            if (key.equals("ingredient" + i)) {
+                ItemStack[] stack = recipe.input.get(i).getMatchingStacksClient();
+                return stack.length > 0 ? IVariable.from(stack[0]) : null;
+            }
+        }
+        return null;
+    }
+}
