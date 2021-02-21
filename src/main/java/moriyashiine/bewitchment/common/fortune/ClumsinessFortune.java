14323b972ed7d570330eceb8ae2fe9693e280187
Aelpecyem <aelpecyem@gmail.com>
2021-02-21 14:47:15 +0100

    cleanup also fix clumsiness dupe

--- src/main/java/moriyashiine/bewitchment/common/fortune/ClumsinessFortune.java
+++ src/main/java/moriyashiine/bewitchment/common/fortune/ClumsinessFortune.java
@@ -2,7 +2,9 @@ package moriyashiine.bewitchment.common.fortune;
 
 import moriyashiine.bewitchment.api.registry.Fortune;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
 
 public class ClumsinessFortune extends Fortune {
 	public ClumsinessFortune(boolean positive) {
@@ -11,6 +13,11 @@ public class ClumsinessFortune extends Fortune {
 	
 	@Override
 	public boolean finish(ServerWorld world, PlayerEntity target) {
-		return target.dropItem(target.getMainHandStack(), false, true) != null;
+		boolean drop = target.dropItem(target.getMainHandStack(), false, true) != null;
+		if(drop){
+			target.setStackInHand(Hand.MAIN_HAND, ItemStack.EMPTY);
+			return true;
+		}
+		return false;
 	}
 }
