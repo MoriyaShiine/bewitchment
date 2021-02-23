ddffeab21f6026b905d8c65dee9c6a944c90cf51
MoriyaShiine <moriyashiine@gmail.com>
2021-02-23 12:12:27 -0600

    clean werewolf model

--- src/main/java/moriyashiine/bewitchment/client/model/entity/living/WerewolfEntityModel.java
+++ src/main/java/moriyashiine/bewitchment/client/model/entity/living/WerewolfEntityModel.java
@@ -13,65 +13,12 @@ import net.minecraft.util.math.MathHelper;
 @Environment(EnvType.CLIENT)
 public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityModel<T> {
 	private final ModelPart body;
-	private final ModelPart head;
-	private final ModelPart stomach;
 	private final ModelPart tail01;
-	private final ModelPart tail02;
-	private final ModelPart tail03;
-	private final ModelPart tail04;
-	private final ModelPart fur06;
-	private final ModelPart fur05;
-	private final ModelPart fur04;
-	private final ModelPart fur03;
-	private final ModelPart fur02;
 	private final ModelPart BipedLeftLeg;
-	private final ModelPart lLeg02;
-	private final ModelPart lLeg03;
-	private final ModelPart lFoot;
-	private final ModelPart lFootClaw01;
-	private final ModelPart lFootClaw02;
-	private final ModelPart lFootClaw03;
 	private final ModelPart BipedRightLeg;
-	private final ModelPart rLeg02;
-	private final ModelPart rLeg03;
-	private final ModelPart rFoot;
-	private final ModelPart rFootClaw01;
-	private final ModelPart rFootClaw02;
-	private final ModelPart rFootClaw03;
 	private final ModelPart neck;
-	private final ModelPart head2;
-	private final ModelPart jawUpperLeft;
-	private final ModelPart upperTeethLeft01;
-	private final ModelPart upperTeethLeft02;
-	private final ModelPart jawUpperRight;
-	private final ModelPart upperTeethRight;
-	private final ModelPart jawLower;
-	private final ModelPart lowerTeeth01;
-	private final ModelPart lowerTeeth02;
-	private final ModelPart lEar01;
-	private final ModelPart lEar02;
-	private final ModelPart rEar01;
-	private final ModelPart rEar02;
-	private final ModelPart lCheekFur;
-	private final ModelPart rCheekFur;
-	private final ModelPart snout;
-	private final ModelPart fur01;
 	private final ModelPart lArm01;
-	private final ModelPart lArm02;
-	private final ModelPart lArmFur;
-	private final ModelPart lClawJoint;
-	private final ModelPart lClaw01;
-	private final ModelPart lClaw02;
-	private final ModelPart lClaw03;
-	private final ModelPart leftHandItem;
 	private final ModelPart rArm01;
-	private final ModelPart rArm02;
-	private final ModelPart rArmFur;
-	private final ModelPart rClawJoint;
-	private final ModelPart rClaw01;
-	private final ModelPart rClaw02;
-	private final ModelPart rClaw03;
-	private final ModelPart rightHandItem;
 	
 	private boolean realArm = false;
 	
@@ -81,12 +28,12 @@ public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityMo
 		setRotation(body, 0.5672F, 0.0F, 0.0F);
 		body.setTextureOffset(49, 15).addCuboid(-5.5F, -3.3F, -2.0F, 11.0F, 11.0F, 8.0F, 0.0F, false);
 		
-		head = new ModelPart(this);
+		ModelPart head = new ModelPart(this);
 		head.setPivot(0.0F, -2.0F, 2.0F);
 		body.addChild(head);
 		head.setTextureOffset(1, 2).addCuboid(-0.5F, 0.7F, -0.4F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 		
-		stomach = new ModelPart(this);
+		ModelPart stomach = new ModelPart(this);
 		stomach.setPivot(0.0F, 6.9F, 2.6F);
 		body.addChild(stomach);
 		setRotation(stomach, -0.3927F, 0.0F, 0.0F);
@@ -98,49 +45,49 @@ public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityMo
 		setRotation(tail01, 0.1047F, 0.0F, 0.0F);
 		tail01.setTextureOffset(112, 18).addCuboid(-1.5F, 0.0F, -1.5F, 3.0F, 4.0F, 3.0F, 0.0F, false);
 		
-		tail02 = new ModelPart(this);
+		ModelPart tail02 = new ModelPart(this);
 		tail02.setPivot(0.0F, 3.7F, 0.0F);
 		tail01.addChild(tail02);
 		setRotation(tail02, -0.2094F, 0.0F, 0.0F);
 		tail02.setTextureOffset(111, 26).addCuboid(-2.0F, 0.0F, -2.0F, 4.0F, 7.0F, 4.0F, 0.0F, false);
 		
-		tail03 = new ModelPart(this);
+		ModelPart tail03 = new ModelPart(this);
 		tail03.setPivot(0.0F, 6.5F, 0.1F);
 		tail02.addChild(tail03);
 		setRotation(tail03, 0.1396F, 0.0F, 0.0F);
 		tail03.setTextureOffset(112, 40).addCuboid(-1.5F, 0.2F, -1.5F, 3.0F, 4.0F, 3.0F, 0.0F, false);
 		
-		tail04 = new ModelPart(this);
+		ModelPart tail04 = new ModelPart(this);
 		tail04.setPivot(0.0F, 0.0F, 0.4F);
 		tail03.addChild(tail04);
 		setRotation(tail04, 0.0698F, 0.0F, 0.0F);
 		tail04.setTextureOffset(116, 7).addCuboid(-1.0F, 0.0F, -1.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
 		
-		fur06 = new ModelPart(this);
+		ModelPart fur06 = new ModelPart(this);
 		fur06.setPivot(0.0F, 6.6F, 1.5F);
 		stomach.addChild(fur06);
 		setRotation(fur06, 0.6981F, 0.0F, 0.0F);
 		fur06.setTextureOffset(90, 52).addCuboid(-2.0F, -1.0F, 0.0F, 4.0F, 5.0F, 2.0F, 0.0F, false);
 		
-		fur05 = new ModelPart(this);
+		ModelPart fur05 = new ModelPart(this);
 		fur05.setPivot(0.0F, 4.7F, 1.6F);
 		stomach.addChild(fur05);
 		setRotation(fur05, 0.6981F, 0.0F, 0.0F);
 		fur05.setTextureOffset(90, 43).addCuboid(-2.0F, -1.0F, 0.0F, 4.0F, 5.0F, 2.0F, 0.0F, false);
 		
-		fur04 = new ModelPart(this);
+		ModelPart fur04 = new ModelPart(this);
 		fur04.setPivot(0.0F, -0.3F, 4.9F);
 		body.addChild(fur04);
 		setRotation(fur04, 0.4363F, 0.0F, 0.0F);
 		fur04.setTextureOffset(90, 35).addCuboid(-2.5F, 1.0F, 0.0F, 5.0F, 4.0F, 2.0F, 0.0F, false);
 		
-		fur03 = new ModelPart(this);
+		ModelPart fur03 = new ModelPart(this);
 		fur03.setPivot(0.0F, -2.6F, 4.5F);
 		body.addChild(fur03);
 		setRotation(fur03, 0.6807F, 0.0F, 0.0F);
 		fur03.setTextureOffset(90, 24).addCuboid(-4.0F, 0.0F, -1.0F, 8.0F, 7.0F, 2.0F, 0.0F, false);
 		
-		fur02 = new ModelPart(this);
+		ModelPart fur02 = new ModelPart(this);
 		fur02.setPivot(0.0F, -4.7F, 3.4F);
 		body.addChild(fur02);
 		setRotation(fur02, 0.8727F, 0.0F, 0.0F);
@@ -151,37 +98,37 @@ public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityMo
 		BipedLeftLeg.setTextureOffset(11, 46).addCuboid(-2.3F, -1.1F, -1.9F, 5.0F, 13.0F, 5.0F, 0.0F, false);
 		stomach.addChild(BipedLeftLeg);
 		
-		lLeg02 = new ModelPart(this);
+		ModelPart lLeg02 = new ModelPart(this);
 		lLeg02.setPivot(0.0F, 9.8F, 0.5F);
 		BipedLeftLeg.addChild(lLeg02);
 		setRotation(lLeg02, 1.309F, -0.0524F, 0.0F);
 		lLeg02.setTextureOffset(0, 35).addCuboid(-2.01F, 0.4F, -2.0F, 4.0F, 7.0F, 4.0F, 0.0F, false);
 		
-		lLeg03 = new ModelPart(this);
+		ModelPart lLeg03 = new ModelPart(this);
 		lLeg03.setPivot(0.0F, 5.9F, 0.8F);
 		lLeg02.addChild(lLeg03);
 		setRotation(lLeg03, -0.7854F, 0.0F, 0.0873F);
 		lLeg03.setTextureOffset(0, 22).addCuboid(-1.5F, 0.0F, -1.5F, 3.0F, 10.0F, 3.0F, 0.0F, false);
 		
-		lFoot = new ModelPart(this);
+		ModelPart lFoot = new ModelPart(this);
 		lFoot.setPivot(0.0F, 8.7F, -1.2F);
 		lLeg03.addChild(lFoot);
 		setRotation(lFoot, 0.1309F, 0.0436F, -0.0087F);
 		lFoot.setTextureOffset(0, 14).addCuboid(-2.0F, 0.0F, -2.8F, 4.0F, 2.0F, 5.0F, 0.0F, false);
 		
-		lFootClaw01 = new ModelPart(this);
+		ModelPart lFootClaw01 = new ModelPart(this);
 		lFootClaw01.setPivot(-1.3F, 0.5F, -2.6F);
 		lFoot.addChild(lFootClaw01);
 		setRotation(lFootClaw01, 0.2269F, 0.1047F, 0.0F);
 		lFootClaw01.setTextureOffset(1, 48).addCuboid(-0.5F, -0.5F, -1.7F, 1.0F, 2.0F, 3.0F, 0.0F, false);
 		
-		lFootClaw02 = new ModelPart(this);
+		ModelPart lFootClaw02 = new ModelPart(this);
 		lFootClaw02.setPivot(0.0F, 0.5F, -2.6F);
 		lFoot.addChild(lFootClaw02);
 		setRotation(lFootClaw02, 0.2269F, 0.0F, 0.0F);
 		lFootClaw02.setTextureOffset(1, 48).addCuboid(-0.5F, -0.5F, -1.7F, 1.0F, 2.0F, 3.0F, 0.0F, false);
 		
-		lFootClaw03 = new ModelPart(this);
+		ModelPart lFootClaw03 = new ModelPart(this);
 		lFootClaw03.setPivot(1.3F, 0.5F, -2.6F);
 		lFoot.addChild(lFootClaw03);
 		setRotation(lFootClaw03, 0.2269F, -0.1047F, 0.0F);
@@ -192,37 +139,37 @@ public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityMo
 		BipedRightLeg.setTextureOffset(11, 46).addCuboid(-2.7F, -1.1F, -1.9F, 5.0F, 13.0F, 5.0F, 0.0F, true);
 		stomach.addChild(BipedRightLeg);
 		
-		rLeg02 = new ModelPart(this);
+		ModelPart rLeg02 = new ModelPart(this);
 		rLeg02.setPivot(0.0F, 9.8F, 0.5F);
 		BipedRightLeg.addChild(rLeg02);
 		setRotation(rLeg02, 1.309F, 0.0524F, 0.0F);
 		rLeg02.setTextureOffset(0, 35).addCuboid(-1.99F, 0.4F, -2.0F, 4.0F, 7.0F, 4.0F, 0.0F, true);
 		
-		rLeg03 = new ModelPart(this);
+		ModelPart rLeg03 = new ModelPart(this);
 		rLeg03.setPivot(0.0F, 5.9F, 0.8F);
 		rLeg02.addChild(rLeg03);
 		setRotation(rLeg03, -0.7854F, 0.0F, -0.0873F);
 		rLeg03.setTextureOffset(0, 22).addCuboid(-1.5F, 0.0F, -1.5F, 3.0F, 10.0F, 3.0F, 0.0F, true);
 		
-		rFoot = new ModelPart(this);
+		ModelPart rFoot = new ModelPart(this);
 		rFoot.setPivot(0.0F, 8.7F, -1.2F);
 		rLeg03.addChild(rFoot);
 		setRotation(rFoot, 0.1309F, -0.0436F, 0.0087F);
 		rFoot.setTextureOffset(0, 14).addCuboid(-2.0F, 0.0F, -2.8F, 4.0F, 2.0F, 5.0F, 0.0F, true);
 		
-		rFootClaw01 = new ModelPart(this);
+		ModelPart rFootClaw01 = new ModelPart(this);
 		rFootClaw01.setPivot(1.3F, 0.5F, -2.6F);
 		rFoot.addChild(rFootClaw01);
 		setRotation(rFootClaw01, 0.2269F, -0.1047F, 0.0F);
 		rFootClaw01.setTextureOffset(1, 48).addCuboid(-0.5F, -0.5F, -1.7F, 1.0F, 2.0F, 3.0F, 0.0F, true);
 		
-		rFootClaw02 = new ModelPart(this);
+		ModelPart rFootClaw02 = new ModelPart(this);
 		rFootClaw02.setPivot(0.0F, 0.5F, -2.6F);
 		rFoot.addChild(rFootClaw02);
 		setRotation(rFootClaw02, 0.2269F, 0.0F, 0.0F);
 		rFootClaw02.setTextureOffset(1, 48).addCuboid(-0.5F, -0.5F, -1.7F, 1.0F, 2.0F, 3.0F, 0.0F, true);
 		
-		rFootClaw03 = new ModelPart(this);
+		ModelPart rFootClaw03 = new ModelPart(this);
 		rFootClaw03.setPivot(-1.3F, 0.5F, -2.6F);
 		rFoot.addChild(rFootClaw03);
 		setRotation(rFootClaw03, 0.2269F, 0.1047F, 0.0F);
@@ -232,98 +179,98 @@ public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityMo
 		setRotation(neck, -0.2531F, 0.0F, 0.0F);
 		neck.setTextureOffset(0, 0).addCuboid(-3.5F, -1.5F, -1.0F, 7.0F, 5.0F, 5.0F, 0.0F, false);
 		
-		head2 = new ModelPart(this);
+		ModelPart head2 = new ModelPart(this);
 		head2.setPivot(0.0F, -0.5F, -3.6F);
 		neck.addChild(head2);
 		setRotation(head2, 1.9199F, 0.0F, 0.0F);
 		head2.setTextureOffset(44, 0).addCuboid(-4.0F, -3.0F, -4.4F, 8.0F, 6.0F, 7.0F, 0.0F, false);
 		
-		jawUpperLeft = new ModelPart(this);
+		ModelPart jawUpperLeft = new ModelPart(this);
 		jawUpperLeft.setPivot(1.2F, -5.1F, -1.5F);
 		head2.addChild(jawUpperLeft);
 		setRotation(jawUpperLeft, 0.0F, 0.0F, -0.1396F);
 		jawUpperLeft.setTextureOffset(20, 36).addCuboid(-1.0F, -2.0F, -2.0F, 2.0F, 5.0F, 2.0F, 0.0F, false);
 		
-		upperTeethLeft01 = new ModelPart(this);
+		ModelPart upperTeethLeft01 = new ModelPart(this);
 		upperTeethLeft01.setPivot(0.4F, -2.8F, -1.0F);
 		jawUpperLeft.addChild(upperTeethLeft01);
 		upperTeethLeft01.setTextureOffset(56, 37).addCuboid(-0.5F, 1.0F, -1.8F, 1.0F, 4.0F, 2.0F, 0.0F, false);
 		
-		upperTeethLeft02 = new ModelPart(this);
+		ModelPart upperTeethLeft02 = new ModelPart(this);
 		upperTeethLeft02.setPivot(0.0F, -3.2F, -1.0F);
 		jawUpperLeft.addChild(upperTeethLeft02);
 		setRotation(upperTeethLeft02, 0.0F, 0.0F, 0.1367F);
 		upperTeethLeft02.setTextureOffset(63, 38).addCuboid(-2.27F, 1.4F, -1.4F, 3.0F, 0.0F, 1.0F, 0.0F, false);
 		
-		jawUpperRight = new ModelPart(this);
+		ModelPart jawUpperRight = new ModelPart(this);
 		jawUpperRight.setPivot(-1.2F, -5.1F, -1.5F);
 		head2.addChild(jawUpperRight);
 		setRotation(jawUpperRight, 0.0F, 0.0F, 0.1396F);
 		jawUpperRight.setTextureOffset(20, 36).addCuboid(-1.0F, -2.0F, -2.0F, 2.0F, 5.0F, 2.0F, 0.0F, true);
 		
-		upperTeethRight = new ModelPart(this);
+		ModelPart upperTeethRight = new ModelPart(this);
 		upperTeethRight.setPivot(-0.4F, -2.8F, -1.0F);
 		jawUpperRight.addChild(upperTeethRight);
 		upperTeethRight.setTextureOffset(56, 37).addCuboid(-0.5F, 1.0F, -1.8F, 1.0F, 4.0F, 2.0F, 0.0F, true);
 		
-		jawLower = new ModelPart(this);
+		ModelPart jawLower = new ModelPart(this);
 		jawLower.setPivot(0.0F, -4.1F, -3.0F);
 		head2.addChild(jawLower);
 		jawLower.setTextureOffset(39, 37).addCuboid(-2.0F, -2.9F, -1.5F, 4.0F, 5.0F, 1.0F, 0.0F, false);
 		
-		lowerTeeth01 = new ModelPart(this);
+		ModelPart lowerTeeth01 = new ModelPart(this);
 		lowerTeeth01.setPivot(0.0F, -3.7F, 0.1F);
 		jawLower.addChild(lowerTeeth01);
 		lowerTeeth01.setTextureOffset(63, 41).addCuboid(-1.6F, 1.3F, -0.6F, 1.0F, 3.0F, 1.0F, 0.0F, false);
 		
-		lowerTeeth02 = new ModelPart(this);
+		ModelPart lowerTeeth02 = new ModelPart(this);
 		lowerTeeth02.setPivot(0.0F, 0.0F, 0.0F);
 		lowerTeeth01.addChild(lowerTeeth02);
 		lowerTeeth02.setTextureOffset(63, 41).addCuboid(0.6F, 1.3F, -0.7F, 1.0F, 3.0F, 1.0F, 0.0F, false);
 		
-		lEar01 = new ModelPart(this);
+		ModelPart lEar01 = new ModelPart(this);
 		lEar01.setPivot(2.8F, -0.1F, 2.8F);
 		head2.addChild(lEar01);
 		setRotation(lEar01, 0.6981F, 0.1222F, -0.6981F);
 		lEar01.setTextureOffset(70, 0).addCuboid(0.0F, -1.0F, -1.6F, 1.0F, 3.0F, 2.0F, 0.0F, true);
 		
-		lEar02 = new ModelPart(this);
+		ModelPart lEar02 = new ModelPart(this);
 		lEar02.setPivot(-0.3F, -3.0F, -0.5F);
 		lEar01.addChild(lEar02);
 		setRotation(lEar02, 0.2269F, 0.0F, -0.2618F);
 		lEar02.setTextureOffset(78, 0).addCuboid(-1.4F, 2.0F, -1.4F, 1.0F, 5.0F, 1.0F, 0.0F, true);
 		
-		rEar01 = new ModelPart(this);
+		ModelPart rEar01 = new ModelPart(this);
 		rEar01.setPivot(-2.8F, -0.1F, 2.8F);
 		head2.addChild(rEar01);
 		setRotation(rEar01, 0.6981F, -0.1222F, 0.6981F);
 		rEar01.setTextureOffset(70, 0).addCuboid(-1.0F, -1.0F, -1.6F, 1.0F, 3.0F, 2.0F, 0.0F, false);
 		
-		rEar02 = new ModelPart(this);
+		ModelPart rEar02 = new ModelPart(this);
 		rEar02.setPivot(0.3F, -3.0F, -0.5F);
 		rEar01.addChild(rEar02);
 		setRotation(rEar02, 0.2269F, 0.0F, 0.2618F);
 		rEar02.setTextureOffset(78, 0).addCuboid(0.4F, 2.0F, -1.4F, 1.0F, 5.0F, 1.0F, 0.0F, false);
 		
-		lCheekFur = new ModelPart(this);
+		ModelPart lCheekFur = new ModelPart(this);
 		lCheekFur.setPivot(3.5F, -0.5F, -0.6F);
 		head2.addChild(lCheekFur);
 		setRotation(lCheekFur, 0.1222F, -0.0873F, -0.5236F);
 		lCheekFur.setTextureOffset(26, 4).addCuboid(0.0F, -0.8F, -3.3F, 0.0F, 6.0F, 5.0F, 0.0F, true);
 		
-		rCheekFur = new ModelPart(this);
+		ModelPart rCheekFur = new ModelPart(this);
 		rCheekFur.setPivot(-3.5F, -0.5F, -0.6F);
 		head2.addChild(rCheekFur);
 		setRotation(rCheekFur, 0.1222F, 0.0873F, 0.5236F);
 		rCheekFur.setTextureOffset(26, 4).addCuboid(0.0F, -0.8F, -3.3F, 0.0F, 6.0F, 5.0F, 0.0F, false);
 		
-		snout = new ModelPart(this);
+		ModelPart snout = new ModelPart(this);
 		snout.setPivot(0.0F, -4.2F, -0.1F);
 		head2.addChild(snout);
 		setRotation(snout, 0.182F, 0.0F, 0.0F);
 		snout.setTextureOffset(29, 35).addCuboid(-1.5F, -3.2F, -2.0F, 3.0F, 5.0F, 2.0F, 0.0F, false);
 		
-		fur01 = new ModelPart(this);
+		ModelPart fur01 = new ModelPart(this);
 		fur01.setPivot(0.0F, 0.0F, -2.9F);
 		neck.addChild(fur01);
 		setRotation(fur01, 1.7453F, 0.0F, 0.0F);
@@ -332,86 +279,78 @@ public class WerewolfEntityModel<T extends WerewolfEntity> extends BipedEntityMo
 		lArm01 = new ModelPart(this);
 		lArm01.setTextureOffset(32, 47).addCuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 		
-		lArm02 = new ModelPart(this);
+		ModelPart lArm02 = new ModelPart(this);
 		lArm02.setPivot(0.0F, 11.5F, 0.0F);
 		lArm01.addChild(lArm02);
 		setRotation(lArm02, -0.5236F, 0.0F, 0.1484F);
 		lArm02.setTextureOffset(49, 46).addCuboid(-1.5F, -1.0F, -1.5F, 3.0F, 13.0F, 3.0F, 0.0F, false);
 		
-		lArmFur = new ModelPart(this);
+		ModelPart lArmFur = new ModelPart(this);
 		lArmFur.setPivot(0.4F, -4.0F, 0.0F);
 		lArm02.addChild(lArmFur);
 		setRotation(lArmFur, -0.4363F, 0.0873F, 0.0873F);
 		lArmFur.setTextureOffset(62, 50).addCuboid(-0.5F, -0.5F, 1.5F, 1.0F, 8.0F, 4.0F, 0.0F, false);
 		
-		lClawJoint = new ModelPart(this);
+		ModelPart lClawJoint = new ModelPart(this);
 		lClawJoint.setPivot(0.0F, 8.6F, 0.0F);
 		lArm02.addChild(lClawJoint);
 		lClawJoint.setTextureOffset(0, 0).addCuboid(-0.4F, 1.5F, -0.5F, 1.0F, 1.0F, 1.0F, 0.0F, false);
 		
-		lClaw01 = new ModelPart(this);
+		ModelPart lClaw01 = new ModelPart(this);
 		lClaw01.setPivot(1.0F, 0.2F, 0.0F);
 		lClawJoint.addChild(lClaw01);
 		setRotation(lClaw01, -0.1047F, 0.0F, 0.2269F);
 		lClaw01.setTextureOffset(27, 0).addCuboid(-1.4F, 1.2F, -1.6F, 2.0F, 5.0F, 1.0F, 0.0F, false);
 		
-		lClaw02 = new ModelPart(this);
+		ModelPart lClaw02 = new ModelPart(this);
 		lClaw02.setPivot(1.0F, 0.2F, -0.1F);
 		lClawJoint.addChild(lClaw02);
 		setRotation(lClaw02, 0.0F, 0.0F, 0.2269F);
 		lClaw02.setTextureOffset(27, 0).addCuboid(-1.4F, 1.2F, -0.5F, 2.0F, 5.0F, 1.0F, 0.0F, false);
 		
-		lClaw03 = new ModelPart(this);
+		ModelPart lClaw03 = new ModelPart(this);
 		lClaw03.setPivot(1.0F, 0.2F, 0.8F);
 		lClawJoint.addChild(lClaw03);
 		setRotation(lClaw03, 0.1047F, 0.0F, 0.2269F);
 		lClaw03.setTextureOffset(27, 0).addCuboid(-1.4F, 1.2F, -0.5F, 2.0F, 5.0F, 1.0F, 0.0F, false);
 		
-		leftHandItem = new ModelPart(this);
-		leftHandItem.setPivot(-1.0F, 8.5F, -0.5F);
-		lArm02.addChild(leftHandItem);
-		
 		rArm01 = new ModelPart(this);
 		rArm01.setTextureOffset(32, 47).addCuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, true);
 		
-		rArm02 = new ModelPart(this);
+		ModelPart rArm02 = new ModelPart(this);
 		rArm02.setPivot(0.0F, 11.5F, 0.0F);
 		rArm01.addChild(rArm02);
 		setRotation(rArm02, -0.5236F, 0.0F, -0.1484F);
 		rArm02.setTextureOffset(49, 46).addCuboid(-1.5F, -1.0F, -1.5F, 3.0F, 13.0F, 3.0F, 0.0F, true);
 		
-		rArmFur = new ModelPart(this);
+		ModelPart rArmFur = new ModelPart(this);
 		rArmFur.setPivot(-0.4F, -4.0F, 0.0F);
 		rArm02.addChild(rArmFur);
 		setRotation(rArmFur, -0.4363F, -0.0873F, -0.0873F);
 		rArmFur.setTextureOffset(62, 50).addCuboid(-0.5F, -0.5F, 1.5F, 1.0F, 8.0F, 4.0F, 0.0F, true);
 		
-		rClawJoint = new ModelPart(this);
+		ModelPart rClawJoint = new ModelPart(this);
 		rClawJoint.setPivot(0.0F, 8.6F, 0.0F);
 		rArm02.addChild(rClawJoint);
 		rClawJoint.setTextureOffset(0, 0).addCuboid(-0.6F, 1.5F, -0.5F, 1.0F, 1.0F, 1.0F, 0.0F, true);
 		
-		rClaw01 = new ModelPart(this);
+		ModelPart rClaw01 = new ModelPart(this);
 		rClaw01.setPivot(-1.0F, 0.2F, 0.0F);
 		rClawJoint.addChild(rClaw01);
 		setRotation(rClaw01, -0.1047F, 0.0F, -0.2269F);
 		rClaw01.setTextureOffset(27, 0).addCuboid(-0.6F, 1.2F, -1.6F, 2.0F, 5.0F, 1.0F, 0.0F, true);
 		
-		rClaw02 = new ModelPart(this);
+		ModelPart rClaw02 = new ModelPart(this);
 		rClaw02.setPivot(-1.0F, 0.2F, -0.1F);
 		rClawJoint.addChild(rClaw02);
 		setRotation(rClaw02, 0.0F, 0.0F, -0.2269F);
 		rClaw02.setTextureOffset(27, 0).addCuboid(-0.6F, 1.2F, -0.5F, 2.0F, 5.0F, 1.0F, 0.0F, true);
 		
-		rClaw03 = new ModelPart(this);
+		ModelPart rClaw03 = new ModelPart(this);
 		rClaw03.setPivot(-1.0F, 0.2F, 0.8F);
 		rClawJoint.addChild(rClaw03);
 		setRotation(rClaw03, 0.1047F, 0.0F, -0.2269F);
 		rClaw03.setTextureOffset(27, 0).addCuboid(-0.6F, 1.2F, -0.5F, 2.0F, 5.0F, 1.0F, 0.0F, true);
-		
-		rightHandItem = new ModelPart(this);
-		rightHandItem.setPivot(1.0F, 8.5F, -0.5F);
-		rArm02.addChild(rightHandItem);
 	}
 	
 	@Override
