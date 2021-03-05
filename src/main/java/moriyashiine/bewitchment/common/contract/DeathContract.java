0f862c7bfb5c1e49bd92bccce758eb388b33161e
MoriyaShiine <moriyashiine@gmail.com>
2021-03-05 17:05:14 -0600

    nazar now protects against death contract curse recoil thing

--- src/main/java/moriyashiine/bewitchment/common/contract/DeathContract.java
+++ src/main/java/moriyashiine/bewitchment/common/contract/DeathContract.java
@@ -1,12 +1,17 @@
 package moriyashiine.bewitchment.common.contract;
 
+import dev.emi.trinkets.api.TrinketsApi;
 import moriyashiine.bewitchment.api.interfaces.entity.ContractAccessor;
 import moriyashiine.bewitchment.api.interfaces.entity.CurseAccessor;
 import moriyashiine.bewitchment.api.registry.Contract;
 import moriyashiine.bewitchment.api.registry.Curse;
 import moriyashiine.bewitchment.common.registry.BWDamageSources;
+import moriyashiine.bewitchment.common.registry.BWObjects;
 import moriyashiine.bewitchment.common.registry.BWRegistries;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.Collections;
 
 public class DeathContract extends Contract {
 	public DeathContract(boolean canBeGiven) {
@@ -26,7 +31,7 @@ public class DeathContract extends Contract {
 			while (curse == null || curse.type != Curse.Type.LESSER) {
 				curse = BWRegistries.CURSES.get(user.getRandom().nextInt(BWRegistries.CURSES.getEntries().size()));
 			}
-			((CurseAccessor) user).addCurse(new Curse.Instance(curse, 24000));
+			((CurseAccessor) user).addCurse(new Curse.Instance(curse, user instanceof PlayerEntity && TrinketsApi.getTrinketsInventory((PlayerEntity) user).containsAny(Collections.singleton(BWObjects.NAZAR)) ? 12000 : 24000));
 		}
 	}
 }
