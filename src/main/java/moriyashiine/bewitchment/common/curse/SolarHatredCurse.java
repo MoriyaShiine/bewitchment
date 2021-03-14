5346a89b3358115a715496928774f2cd1562451c
MoriyaShiine <moriyashiine@gmail.com>
2021-03-14 14:30:23 -0500

    nerf solar hatred

--- src/main/java/moriyashiine/bewitchment/common/curse/SolarHatredCurse.java
+++ src/main/java/moriyashiine/bewitchment/common/curse/SolarHatredCurse.java
@@ -14,8 +14,8 @@ public class SolarHatredCurse extends Curse {
 		if (target instanceof RespawnTimerAccessor && ((RespawnTimerAccessor) target).getRespawnTimer() > 0) {
 			return;
 		}
-		if (target.getFireTicks() <= 1 && target.world.isDay() && target.world.isSkyVisible(target.getBlockPos())) {
-			target.setOnFireFor(1);
+		if (target.age % 600 == 0 && target.world.isDay() && target.world.isSkyVisible(target.getBlockPos())) {
+			target.setOnFireFor(8);
 		}
 	}
 }
