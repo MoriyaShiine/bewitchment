6e2b5fbc7fe27d4817652ba5460e8f858d8642f7
MoriyaShiine <moriyashiine@gmail.com>
2021-03-20 09:24:36 -0500

    merge greed and pride contracts

--- src/main/java/moriyashiine/bewitchment/mixin/contract/BlockMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/contract/BlockMixin.java
@@ -9,16 +9,12 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.world.explosion.Explosion;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -39,31 +35,21 @@ public abstract class BlockMixin {
 					boolean foundOre = false;
 					for (int i = 0; i < drops.size(); i++) {
 						if (BWTags.ORES.contains(state.getBlock().asItem())) {
+							for (Recipe<?> smeltingRecipe : world.getRecipeManager().listAllOfType(RecipeType.SMELTING)) {
+								if (BWTags.ORES.contains(drops.get(i).getItem())) {
+									for (Ingredient ingredient : smeltingRecipe.getPreviewInputs()) {
+										if (ingredient.test(drops.get(i))) {
+											drops.set(i, new ItemStack(smeltingRecipe.getOutput().getItem(), smeltingRecipe.getOutput().getCount() * drops.get(i).getCount()));
+										}
+									}
+								}
+							}
 							drops.set(i, new ItemStack(drops.get(i).getItem(), drops.get(i).getCount() * 2));
 							foundOre = true;
 						}
 					}
 					if (foundOre && contractAccessor.hasNegativeEffects() && world.random.nextFloat() < 1 / 3f) {
 						drops.clear();
-					}
-				}
-				if (contractAccessor.hasContract(BWContracts.PRIDE)) {
-					boolean foundOre = false;
-					for (Recipe<?> smeltingRecipe : world.getRecipeManager().listAllOfType(RecipeType.SMELTING)) {
-						for (int i = 0; i < drops.size(); i++) {
-							if (BWTags.ORES.contains(drops.get(i).getItem())) {
-								for (Ingredient ingredient : smeltingRecipe.getPreviewInputs()) {
-									if (ingredient.test(drops.get(i))) {
-										drops.set(i, new ItemStack(smeltingRecipe.getOutput().getItem(), smeltingRecipe.getOutput().getCount() * drops.get(i).getCount()));
-										foundOre = true;
-									}
-								}
-							}
-						}
-					}
-					if (foundOre && contractAccessor.hasNegativeEffects() && world.random.nextFloat() < 1 / 8f) {
-						world.createExplosion(entity, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 3, Explosion.DestructionType.BREAK);
-						world.getEntitiesByClass(LivingEntity.class, new Box(pos).expand(4), LivingEntity::isAlive).forEach(livingEntity -> livingEntity.damage(DamageSource.explosion((LivingEntity) entity), 12));
 					}
 				}
 			}
