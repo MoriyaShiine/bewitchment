7294bd5d32ee799beefe3c11c4cb7901b6288b88
MoriyaShiine <moriyashiine@gmail.com>
2021-03-20 11:38:14 -0500

    pride has a new effect

--- src/main/java/moriyashiine/bewitchment/mixin/contract/PlayerEntityMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/contract/PlayerEntityMixin.java
@@ -4,22 +4,27 @@ import moriyashiine.bewitchment.api.interfaces.entity.ContractAccessor;
 import moriyashiine.bewitchment.common.registry.BWContracts;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements ContractAccessor {
 	@Shadow
 	public abstract HungerManager getHungerManager();
+	
+	@Shadow public abstract boolean damage(DamageSource source, float amount);
 	
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
@@ -31,6 +36,22 @@ public abstract class PlayerEntityMixin extends LivingEntity implements Contract
 			exhaustion *= 1.5f;
 		}
 		return exhaustion;
+	}
+	
+	@ModifyVariable(method = "addExperience", at = @At("HEAD"))
+	private int modifyAddExperience(int experience) {
+		if (hasContract(BWContracts.PRIDE)) {
+			experience *= 2;
+		}
+		return experience;
+	}
+	
+	@Inject(method = "addExperience", at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/entity/player/PlayerEntity;addExperienceLevels(I)V"))
+	private void addExperience(int experience, CallbackInfo callbackInfo) {
+		if (!world.isClient && hasContract(BWContracts.PRIDE) && hasNegativeEffects()) {
+			damage(DamageSource.explosion(this), getMaxHealth() / 2);
+			world.createExplosion(this, getX(), getY(), getZ(), 2, Explosion.DestructionType.NONE);
+		}
 	}
 	
 	@Inject(method = "eatFood", at = @At("HEAD"))
