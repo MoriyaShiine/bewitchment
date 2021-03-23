14a49c54a2b154b84b6977d4d498ea8d6debd5c3
MoriyaShiine <moriyashiine@gmail.com>
2021-03-23 14:58:37 -0500

    very dumb

--- src/main/java/moriyashiine/bewitchment/mixin/familiar/SwoopMovementGoalMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/familiar/SwoopMovementGoalMixin.java
@@ -3,6 +3,7 @@ package moriyashiine.bewitchment.mixin.familiar;
 import moriyashiine.bewitchment.api.BewitchmentAPI;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(targets = "net.minecraft.entity.mob.PhantomEntity$SwoopMovementGoal")
-public class PhantomEntitySwoopMovementGoalMixin {
+@Mixin(PhantomEntity.SwoopMovementGoal.class)
+public class SwoopMovementGoalMixin {
 	@Inject(method = "shouldContinue", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isAlive()Z"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
 	private void shouldContinue(CallbackInfoReturnable<Boolean> callbackInfo, LivingEntity livingEntity) {
 		if (livingEntity instanceof PlayerEntity && BewitchmentAPI.getFamiliar((PlayerEntity) livingEntity) == EntityType.CAT) {
