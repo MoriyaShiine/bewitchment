3b792168d599cc296de28e774f4d9785a09bc5dc
Aelpecyem <aelpecyem@gmail.com>
2021-04-27 18:56:24 +0200

    fix #90

--- src/main/java/moriyashiine/bewitchment/client/misc/SpriteIdentifiers.java
+++ src/main/java/moriyashiine/bewitchment/client/misc/SpriteIdentifiers.java
@@ -3,10 +3,12 @@ package moriyashiine.bewitchment.client.misc;
 import moriyashiine.bewitchment.common.Bewitchment;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
 
 import static net.minecraft.client.render.TexturedRenderLayers.CHEST_ATLAS_TEXTURE;
+import static net.minecraft.client.texture.SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE;
 
 @Environment(EnvType.CLIENT)
 public class SpriteIdentifiers {
@@ -37,4 +39,6 @@ public class SpriteIdentifiers {
 	public static final SpriteIdentifier TRAPPED_DRAGONS_BLOOD_LEFT = new SpriteIdentifier(CHEST_ATLAS_TEXTURE, new Identifier(Bewitchment.MODID, "entity/chest/trapped_dragons_blood_left"));
 	public static final SpriteIdentifier DRAGONS_BLOOD_RIGHT = new SpriteIdentifier(CHEST_ATLAS_TEXTURE, new Identifier(Bewitchment.MODID, "entity/chest/dragons_blood_right"));
 	public static final SpriteIdentifier TRAPPED_DRAGONS_BLOOD_RIGHT = new SpriteIdentifier(CHEST_ATLAS_TEXTURE, new Identifier(Bewitchment.MODID, "entity/chest/trapped_dragons_blood_right"));
+
+	public static final SpriteIdentifier CAULDRON_WATER = new SpriteIdentifier(BLOCK_ATLAS_TEXTURE, new Identifier("block/water_still"));
 }
