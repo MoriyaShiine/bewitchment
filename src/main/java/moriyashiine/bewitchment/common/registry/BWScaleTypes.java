7bf133a411da3c1bd86ab03b0a763732bf924d55
MoriyaShiine <moriyashiine@gmail.com>
2021-05-06 19:48:59 -0500

    update to pehkui 2.1.0

--- src/main/java/moriyashiine/bewitchment/common/registry/BWScaleTypes.java
+++ src/main/java/moriyashiine/bewitchment/common/registry/BWScaleTypes.java
@@ -2,26 +2,16 @@ package moriyashiine.bewitchment.common.registry;
 
 import moriyashiine.bewitchment.common.Bewitchment;
 import net.minecraft.util.Identifier;
-import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleModifier;
 import virtuoel.pehkui.api.ScaleRegistries;
 import virtuoel.pehkui.api.ScaleType;
+import virtuoel.pehkui.api.TypedScaleModifier;
 
 import java.util.Map;
 
 public class BWScaleTypes {
-	public static final ScaleModifier MODIFY_WIDTH_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, "modify_width", new ScaleModifier() {
-		@Override
-		public float modifyScale(final ScaleData scaleData, float modifiedScale, final float delta) {
-			return MODIFY_WIDTH_TYPE.getScaleData(scaleData.getEntity()).getScale(delta) * modifiedScale;
-		}
-	});
-	public static final ScaleModifier MODIFY_HEIGHT_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, "modify_height", new ScaleModifier() {
-		@Override
-		public float modifyScale(final ScaleData scaleData, float modifiedScale, final float delta) {
-			return MODIFY_HEIGHT_TYPE.getScaleData(scaleData.getEntity()).getScale(delta) * modifiedScale;
-		}
-	});
+	public static final ScaleModifier MODIFY_WIDTH_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, "modify_width", new TypedScaleModifier(() -> BWScaleTypes.MODIFY_WIDTH_TYPE));
+	public static final ScaleModifier MODIFY_HEIGHT_MODIFIER = register(ScaleRegistries.SCALE_MODIFIERS, "modify_height", new TypedScaleModifier(() -> BWScaleTypes.MODIFY_HEIGHT_TYPE));
 	
 	public static final ScaleType MODIFY_WIDTH_TYPE = register(ScaleRegistries.SCALE_TYPES, "modify_width", ScaleType.Builder.create().addDependentModifier(MODIFY_WIDTH_MODIFIER).affectsDimensions().build());
 	public static final ScaleType MODIFY_HEIGHT_TYPE = register(ScaleRegistries.SCALE_TYPES, "modify_height", ScaleType.Builder.create().addDependentModifier(MODIFY_HEIGHT_MODIFIER).affectsDimensions().build());
