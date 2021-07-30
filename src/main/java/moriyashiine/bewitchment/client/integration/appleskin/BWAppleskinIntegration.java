43b9d6f6a803989e901dc715557e2261daffcb1a
MoriyaShiine <moriyashiine@gmail.com>
2021-07-29 20:22:17 -0500

    fix #172

---
+++ src/main/java/moriyashiine/bewitchment/client/integration/appleskin/BWAppleskinIntegration.java
@@ -0,0 +1,22 @@
+package moriyashiine.bewitchment.client.integration.appleskin;
+
+import moriyashiine.bewitchment.api.BewitchmentAPI;
+import net.minecraft.client.MinecraftClient;
+import squeek.appleskin.api.AppleSkinApi;
+import squeek.appleskin.api.event.HUDOverlayEvent;
+
+public class BWAppleskinIntegration implements AppleSkinApi {
+	@Override
+	public void registerEvents() {
+		HUDOverlayEvent.Saturation.EVENT.register(saturation -> {
+			if (BewitchmentAPI.isVampire(MinecraftClient.getInstance().player, true)) {
+				saturation.isCanceled = true;
+			}
+		});
+		HUDOverlayEvent.Exhaustion.EVENT.register(exhaustion -> {
+			if (BewitchmentAPI.isVampire(MinecraftClient.getInstance().player, true)) {
+				exhaustion.isCanceled = true;
+			}
+		});
+	}
+}
