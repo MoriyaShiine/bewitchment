e25e34cd7d7c58fa7408b5c315f0f711e44bde6e
MoriyaShiine <moriyashiine@gmail.com>
2021-08-13 14:07:48 -0500

    vampires now get blood from the saturation effect

---
+++ src/main/java/moriyashiine/bewitchment/mixin/transformation/StatusEffectMixin.java
@@ -0,0 +1,24 @@
+package moriyashiine.bewitchment.mixin.transformation;
+
+import moriyashiine.bewitchment.api.BewitchmentAPI;
+import moriyashiine.bewitchment.api.component.BloodComponent;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(StatusEffect.class)
+public class StatusEffectMixin {
+	@Inject(method = "applyUpdateEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getHungerManager()Lnet/minecraft/entity/player/HungerManager;"))
+	private void applyUpdateEffect(LivingEntity entity, int amplifier, CallbackInfo ci) {
+		if (BewitchmentAPI.isVampire(entity, true)) {
+			BloodComponent.maybeGet(entity).ifPresent(bloodComponent -> {
+				if (bloodComponent.getBlood() < BloodComponent.MAX_BLOOD) {
+					bloodComponent.fillBlood(amplifier + 1, false);
+				}
+			});
+		}
+	}
+}
