c4902578a150828666ad1da4ada29686fce6730a
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2021-09-10 15:55:11 -0500

    rename BlockEntityMixin

--- src/main/java/moriyashiine/bewitchment/mixin/BlockEntityTypeMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/BlockEntityTypeMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @SuppressWarnings("ConstantConditions")
 @Mixin(BlockEntityType.class)
-public class BlockEntityMixin {
+public class BlockEntityTypeMixin {
 	@Inject(method = "supports", at = @At("HEAD"), cancellable = true)
 	private void supports(BlockState state, CallbackInfoReturnable<Boolean> callbackInfo) {
 		if ((Object) this instanceof BedBlockEntity && state.getBlock() instanceof CoffinBlock) {
