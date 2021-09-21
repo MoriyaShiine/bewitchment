da44f5708bbcd184eea9e6e0b7bd25307331813e
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2021-09-21 17:23:19 -0500

    buff hellish bauble

--- src/main/java/moriyashiine/bewitchment/common/item/HellishBaubleItem.java
+++ src/main/java/moriyashiine/bewitchment/common/item/HellishBaubleItem.java
@@ -4,9 +4,10 @@ import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketItem;
 import moriyashiine.bewitchment.api.BewitchmentAPI;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
 
 public class HellishBaubleItem extends TrinketItem {
 	public HellishBaubleItem(Settings settings) {
@@ -15,9 +16,11 @@ public class HellishBaubleItem extends TrinketItem {
 	
 	@Override
 	public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
-		if (entity instanceof PlayerEntity player && !entity.world.isClient && entity.age % 20 == 0 && entity.isOnFire() && BewitchmentAPI.drainMagic(player, 1, false)) {
-			entity.world.playSound(null, entity.getBlockPos(), SoundEvents.ENTITY_GENERIC_EXTINGUISH_FIRE, entity.getSoundCategory(), 1, 1);
-			entity.extinguish();
+		if (entity instanceof PlayerEntity player && !entity.world.isClient && entity.age % 10 == 0 && entity.isOnFire()) {
+			StatusEffectInstance fireResistance = new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 300, 0, true, false);
+			if (entity.canHaveStatusEffect(fireResistance) && !entity.hasStatusEffect(StatusEffects.FIRE_RESISTANCE) && BewitchmentAPI.drainMagic(player, 1, false)) {
+				entity.addStatusEffect(fireResistance);
+			}
 		}
 	}
 }
