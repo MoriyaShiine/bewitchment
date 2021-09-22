0534917e5947ce66a0edd717d8db63debe9462a3
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2021-09-22 08:40:59 -0500

    vampires no longer take double fire damage

--- src/main/java/moriyashiine/bewitchment/mixin/transformation/LivingEntityMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/transformation/LivingEntityMixin.java
@@ -1,7 +1,6 @@
 package moriyashiine.bewitchment.mixin.transformation;
 
 import moriyashiine.bewitchment.api.BewitchmentAPI;
-import moriyashiine.bewitchment.common.registry.BWDamageSources;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
@@ -20,14 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class LivingEntityMixin extends Entity {
 	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
-	}
-	
-	@ModifyVariable(method = "damage", at = @At("HEAD"))
-	private float modifyDamage0(float amount, DamageSource source) {
-		if (source == BWDamageSources.SUN) {
-			amount *= 2;
-		}
-		return amount;
 	}
 	
 	@ModifyVariable(method = "applyArmorToDamage", at = @At("HEAD"))
