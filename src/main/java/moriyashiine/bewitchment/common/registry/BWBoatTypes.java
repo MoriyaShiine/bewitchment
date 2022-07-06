f2b6e76349866015226f9f30f6f67d3d8ecf840a
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-07-05 20:57:17 -0500

    add chest boats

--- src/main/java/moriyashiine/bewitchment/common/registry/BWBoatTypes.java
+++ src/main/java/moriyashiine/bewitchment/common/registry/BWBoatTypes.java
@@ -7,29 +7,27 @@ package moriyashiine.bewitchment.common.registry;
 import com.terraformersmc.terraform.boat.api.TerraformBoatType;
 import com.terraformersmc.terraform.boat.api.TerraformBoatTypeRegistry;
 import com.terraformersmc.terraform.boat.api.item.TerraformBoatItemHelper;
+import com.terraformersmc.terraform.boat.impl.TerraformBoatTypeImpl;
 import moriyashiine.bewitchment.common.Bewitchment;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class BWBoatTypes {
-	public static TerraformBoatType juniper, cypress, elder, dragons_blood;
-
 	public static void init() {
-		Item juniper_boat = TerraformBoatItemHelper.registerBoatItem(new Identifier(Bewitchment.MODID, "juniper_boat"), () -> juniper, false, Bewitchment.BEWITCHMENT_GROUP);
-		juniper = new TerraformBoatType.Builder().item(juniper_boat).build();
-		Registry.register(TerraformBoatTypeRegistry.INSTANCE, new Identifier(Bewitchment.MODID, "juniper"), juniper);
+		registerBoat("juniper");
+		registerBoat("cypress");
+		registerBoat("elder");
+		registerBoat("dragons_blood");
+	}
 
-		Item cypress_boat = TerraformBoatItemHelper.registerBoatItem(new Identifier(Bewitchment.MODID, "cypress_boat"), () -> cypress, false, Bewitchment.BEWITCHMENT_GROUP);
-		cypress = new TerraformBoatType.Builder().item(cypress_boat).build();
-		Registry.register(TerraformBoatTypeRegistry.INSTANCE, new Identifier(Bewitchment.MODID, "cypress"), cypress);
-
-		Item elder_boat = TerraformBoatItemHelper.registerBoatItem(new Identifier(Bewitchment.MODID, "elder_boat"), () -> elder, false, Bewitchment.BEWITCHMENT_GROUP);
-		elder = new TerraformBoatType.Builder().item(elder_boat).build();
-		Registry.register(TerraformBoatTypeRegistry.INSTANCE, new Identifier(Bewitchment.MODID, "elder"), elder);
-
-		Item dragons_blood_boat = TerraformBoatItemHelper.registerBoatItem(new Identifier(Bewitchment.MODID, "dragons_blood_boat"), () -> dragons_blood, false, Bewitchment.BEWITCHMENT_GROUP);
-		dragons_blood = new TerraformBoatType.Builder().item(dragons_blood_boat).build();
-		Registry.register(TerraformBoatTypeRegistry.INSTANCE, new Identifier(Bewitchment.MODID, "dragons_blood"), dragons_blood);
+	private static void registerBoat(String name) {
+		AtomicReference<TerraformBoatType> type = new AtomicReference<>();
+		Item boat = TerraformBoatItemHelper.registerBoatItem(new Identifier(Bewitchment.MODID, name + "_boat"), type::get, false, Bewitchment.BEWITCHMENT_GROUP);
+		Item chest_boat = TerraformBoatItemHelper.registerBoatItem(new Identifier(Bewitchment.MODID, name + "_chest_boat"), type::get, true, Bewitchment.BEWITCHMENT_GROUP);
+		type.set(new TerraformBoatTypeImpl(boat, chest_boat));
+		Registry.register(TerraformBoatTypeRegistry.INSTANCE, new Identifier(Bewitchment.MODID, name), type.get());
 	}
 }
