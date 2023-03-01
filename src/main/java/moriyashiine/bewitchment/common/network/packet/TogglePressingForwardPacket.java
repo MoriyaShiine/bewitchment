f02caa3eb51a3b7ab71fa5f5e3ab4e5f1bf7da82
Raeeeee <73916067+Tors-0@users.noreply.github.com>
2023-02-28 18:23:46 -0800

    added TODO 2/28/2023
    This is only intended as a temporary fix! A better fix would be to change the way ME works so that instead of crashing on values below 0, it simply denies the action, allows ME to go negative, then resets ME to 0 since the action was not allowed

--- src/main/java/moriyashiine/bewitchment/common/network/packet/TogglePressingForwardPacket.java
+++ src/main/java/moriyashiine/bewitchment/common/network/packet/TogglePressingForwardPacket.java
@@ -27,7 +27,7 @@ public class TogglePressingForwardPacket {
 	}
 
 	public static void handle(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler network, PacketByteBuf buf, PacketSender sender) {
-		try {
+		try { // attempt normal runtime of TogglePressingForward
 			boolean pressingForward = buf.readBoolean();
 			server.execute(() -> {
 				if (pressingForward && BewitchmentAPI.getFamiliar(player) != BWEntityTypes.OWL) {
@@ -40,6 +40,7 @@ public class TogglePressingForwardPacket {
 				}
 				BWComponents.BROOM_USER_COMPONENT.get(player).setPressingForward(pressingForward);
 			});
-		} catch (IndexOutOfBoundsException ignored) {}
+		} catch (IndexOutOfBoundsException ignored) {} // Supress client crash when attempting to fly with 0 ME
+		// TODO 2/28/2023 : This is only intended as a temporary fix! A better fix would be to change the way ME works so that instead of crashing on values below 0, it simply denies the action, allows ME to go negative, then resets ME to 0 since the action was not allowed // end TODO
 	}
 }
