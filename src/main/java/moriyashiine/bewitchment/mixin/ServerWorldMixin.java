f0fae4ecd21a3b62243a54ffda510e75e3885f0d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2023-05-09 18:42:23 -0500

    fix #319

--- src/main/java/moriyashiine/bewitchment/mixin/ServerWorldMixin.java
+++ src/main/java/moriyashiine/bewitchment/mixin/ServerWorldMixin.java
@@ -4,39 +4,49 @@
 
 package moriyashiine.bewitchment.mixin;
 
+import moriyashiine.bewitchment.common.block.CoffinBlock;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import java.util.function.BooleanSupplier;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin extends World {
 	@Shadow
-	public abstract void setTimeOfDay(long timeOfDay);
+	@Final
+	List<ServerPlayerEntity> players;
 
 	protected ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> dimension, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
 		super(properties, registryRef, dimension, profiler, isClient, debugWorld, seed, maxChainedNeighborUpdates);
 	}
 
-	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V", shift = At.Shift.AFTER))
-	public void changeCoffinWakeTime(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
-		if (isDay()) {
-			long time = getTimeOfDay() - 24000;
-			while (time % 24000 < 13000) {
-				time++;
+	@ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V"))
+	private long bewitchment$coffinWakeTime(long value) {
+		boolean allInCoffins = true;
+		for (ServerPlayerEntity player : players) {
+			Optional<BlockPos> sleepingPos = player.getSleepingPosition();
+			if (sleepingPos.isPresent() && !(getBlockState(sleepingPos.get()).getBlock() instanceof CoffinBlock)) {
+				allInCoffins = false;
+				break;
 			}
-			setTimeOfDay(time);
 		}
+		if (allInCoffins) {
+			return value - 11000L;
+		}
+		return value;
 	}
 }
